c2d001ad392304ec1f3b7dd516fca90384956af4
krizex <krizex@gmail.com>
2018-10-15 10:11:37 +0800

    comment

--- src/lamp/data/init-data.sh
+++ src/lamp/data/init-data.sh
@@ -2,6 +2,9 @@
 
 while read line
 do
+    if [[ $line == \#* ]]; then
+        continue
+    fi
     IFS=' ' read -r -a array <<< "$line"
     #sqlite3 test.db "select * from abc;" ".exit"
     sqlite3 database.db "insert into candidates values (\"${array[0]}\", ${array[1]}, ${array[2]}, ${array[3]}, ${array[4]}, \"\")"
