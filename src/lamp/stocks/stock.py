5e8aad711ed9094a1b367deec21396cdf10a8834
krizex <krizex@gmail.com>
2018-10-15 21:05:47 +0800

    get latest price
    Signed-off-by: krizex <krizex@gmail.com>

--- src/lamp/stocks/stock.py
+++ src/lamp/stocks/stock.py
@@ -5,11 +5,11 @@ from lamp.utils.util import ndays_before_today
 class Stock(object):
     def __init__(self, code):
         self.code = code
-        self.df = self.get_k_data(ndays_before_today(30).strftime('%Y-%m-%d'), '')
+        self.df = self.get_k_data()
         self.name = StockMgr.get_stock_name(self.code)
 
-    def get_k_data(self, start, end):
-        return ts.get_k_data(self.code, start, end, retry_count=10)
+    def get_k_data(self):
+        return ts.get_k_data(self.code, retry_count=10)
 
     def get_last_day_info(self):
         today = self.df.shape[0] - 1
