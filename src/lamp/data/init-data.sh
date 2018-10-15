5a5f51086e838d759e3f78f175b2f9ca4f4773d6
krizex <krizex@gmail.com>
2018-10-15 21:35:55 +0800

    update data when init
    Signed-off-by: krizex <krizex@gmail.com>

--- src/lamp/data/init-data.sh
+++ src/lamp/data/init-data.sh
@@ -8,4 +8,5 @@ do
     IFS=' ' read -r -a array <<< "$line"
     #sqlite3 test.db "select * from abc;" ".exit"
     sqlite3 database.db "insert into candidates values (\"${array[0]}\", ${array[1]}, ${array[2]}, ${array[3]}, ${array[4]}, \"\")"
+    sqlite3 database.db "update candidates set start_pe=${array[1]}, start_price=${array[2]}, stop_pe=${array[3]}, own=${array[4]} where code=\"${array[0]}\""
 done < source.txt
