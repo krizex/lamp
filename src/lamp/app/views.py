5237cdca717a7c65782382b4f65853a940ade862
Yang Qian <yang.qian@citrix.com>
2018-10-16 13:41:17 +0800

    remove useless code
    Signed-off-by: Yang Qian <yang.qian@citrix.com>

--- src/lamp/app/views.py
+++ src/lamp/app/views.py
@@ -4,40 +4,6 @@ from flask import render_template
 
 from lamp.app.controllers import get_sorted_candidates
 
-__author__ = 'David Qian'
-
-"""
-Created on 12/19/2016
-@author: David Qian
-
-"""
-
-
-def display_sales_data():
-    records = get_sales_data()
-
-    records_date = [str(rec['date']) for rec in records]
-
-    records_date = {
-        'categories': records_date
-    }
-    subscribe = [rec['subscribe'] for rec in records]
-    deal_num = [rec['deal_num'] for rec in records]
-    sales_data = [
-        {
-            'name': 'subscribe',
-            'data': subscribe,
-        },
-        {
-            'name': 'deal',
-            'data': deal_num,
-        },
-    ]
-
-    print records_date
-    print sales_data
-    return render_template('sales_data.html', xAxis=records_date, series=sales_data)
-
 
 def display_candidates_data():
     recs = get_sorted_candidates()
