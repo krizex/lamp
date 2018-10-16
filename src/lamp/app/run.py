313546b38dee4c12b4fc92a5788f165f841a356b
Yang Qian <yang.qian@citrix.com>
2018-10-16 16:30:45 +0800

    remove comments
    Signed-off-by: Yang Qian <yang.qian@citrix.com>

--- src/lamp/app/run.py
+++ src/lamp/app/run.py
@@ -2,13 +2,5 @@
 # -*- coding: utf-8 -*-
 from lamp.app import app
 
-__author__ = 'David Qian'
-
-"""
-Created on 12/20/2016
-@author: David Qian
-
-"""
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, passthrough_errors=True)
