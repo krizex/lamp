228e1267f18dc45d2f98d737ad7d41eb4fb433ce
krizex <krizex@gmail.com>
2018-12-06 17:29:38 +0800

    SharedFile
    Signed-off-by: krizex <krizex@gmail.com>

---
+++ src/lamp/utils/file/sf.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import os
+import fcntl
+import contextlib
+
+
+class SharedFile(object):
+    def __init__(self, name):
+        self.name = name
+
+    def exclusive_create(self, data_f):
+        if self.exist():
+            return
+
+        with open(self.name, 'a') as f:
+            try:
+                fcntl.flock(f, fcntl.LOCK_EX|fcntl.LOCK_NB)
+            except IOError as _ :
+                return
+
+            # now get the lock
+            data = data_f()
+            f.write(data)
+
+    def exist(self):
+        return os.path.isfile(self.name)
+
+    @contextlib.contextmanager
+    def open_read(self):
+        with open(self.name, 'r') as f:
+            fcntl.flock(f, fcntl.LOCK_SH)
+            yield f
+
+
+def test_main():
+    def foo():
+        import time
+        time.sleep(10)
+        return '1234567890\n'
+    import os
+    sf = SharedFile(os.path.expanduser('~/temp/sf'))
+    sf.exclusive_create(foo)
+    with sf.open_read() as f:
+        data = f.read()
+        print data
+
+if __name__ == '__main__':
+    test_main()
