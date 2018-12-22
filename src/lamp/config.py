37cfed25ce188d243c04bebe155b5076cd8bd471
krizex <krizex@gmail.com>
2018-12-22 11:49:29 +0800

    fixup! data
    Signed-off-by: krizex <krizex@gmail.com>

--- src/lamp/config.py
+++ src/lamp/config.py
@@ -3,23 +3,6 @@
 import logging
 import os
 
-
-root_node = {
-    'path': os.path.join(os.path.dirname(__file__), 'running')
-}
-
-data_node = {
-    'path': os.path.join(root_node['path'], 'data')
-}
-
-file_store = {
-    'path': os.path.join(data_node['path'], 'images')
-}
-
-scanner = {
-    'interval': 24 * 3600
-}
-
 data_node = os.path.join(os.path.dirname(__file__), '..', 'data')
 database = os.path.join(data_node, 'database.db')
 basis_persistent_file = os.path.join(data_node, 'basis.json')
