bee8a1e046fe920ffd928e7d915f19a8cb6ef92c
krizex <krizex@gmail.com>
2018-12-27 17:20:22 +0800

    ask for confirmation before refresh data
    Signed-off-by: krizex <krizex@gmail.com>

--- src/refresh-data.py
+++ src/refresh-data.py
@@ -3,7 +3,22 @@
 from lamp.app import app
 from lamp.db.helpers import cli
 
+def confirm():
+    """
+    Ask user to enter Y or N (case-insensitive).
+    :return: True if the answer is Y.
+    :rtype: bool
+    """
+    answer = ""
+    while answer not in ["y", "n"]:
+        answer = raw_input("OK refresh data [Y/N]? ").lower()
+    return answer == "y"
+
 def main():
+    if not confirm():
+        print 'Canceled'
+        return
+
     with app.app_context():
         cli.update_candidates_from_file('data.json')
         cli.update_grids_from_file('grid.json')
