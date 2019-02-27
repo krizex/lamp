646ef8453928af12bcc60e9b336a8dcf91ef4dfb
krizex <krizex@gmail.com>
2019-02-27 10:54:13 +0800

    refine templates
    Signed-off-by: krizex <krizex@gmail.com>

--- src/lamp/app/routers.py
+++ src/lamp/app/routers.py
@@ -8,19 +8,28 @@ from lamp.log import log
 from flask import render_template
 
 
-@app.route('/wave/')
-def index():
-    # cli.update_candidates_from_file('data.json')
-    # log.debug('Candidate refreshed')
-    return display_candidates_data()
-
-
 @app.route('/')
-@app.route('/grid/')
-def grid():
-    # cli.update_grids_from_file('grid.json')
-    # log.debug('Grid refreshed')
+@app.route('/wave/')
+def wave():
     grid_recs = get_grids_data()
     trend_recs = get_trends_data()
     rebound_recs = get_rebounds_data()
-    return render_template('grids_data.html', grid_recs=grid_recs, trend_recs=trend_recs, rebound_recs=rebound_recs)
+    return render_template('wave_page.j2', grid_recs=grid_recs, trend_recs=trend_recs, rebound_recs=rebound_recs)
+
+
+@app.route('/grid/')
+def grid():
+    grid_recs = get_grids_data()
+    return render_template('grid_page.j2', grid_recs=grid_recs)
+
+
+@app.route('/trend/')
+def trend():
+    trend_recs = get_trends_data()
+    return render_template('trend_page.j2', trend_recs=trend_recs)
+
+
+@app.route('/rebound/')
+def rebound():
+    rebound_recs = get_rebounds_data()
+    return render_template('rebound_page.j2', rebound_recs=rebound_recs)
