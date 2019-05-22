f4ba89d90e3c4beb56cb3b3d310fca3e0bc3f789
krizex <krizex@gmail.com>
2019-05-22 23:54:54 +0800

    refine display
    Signed-off-by: krizex <krizex@gmail.com>

--- src/lamp/app/controllers/underestimate.py
+++ src/lamp/app/controllers/underestimate.py
@@ -3,20 +3,24 @@ from collections import namedtuple
 from lamp.utils.util import parallel_apply, constructor_of
 from lamp.app.controllers.base import ObjectBaseUnit
 
-
 class Underestimate(ObjectBaseUnit):
+    TYPE = 'Underestimate'
+
     def __init__(self, name, code, pos):
         super(Underestimate, self).__init__(name, code, 0)
         self.pos = pos
 
     @property
     def last_n_days_date(self):
-        ret = self.get_last_n_days_date(22)
-        return ret
+        return self.get_last_n_days_date(22)
 
     @property
     def last_n_days_price(self):
         return self.get_last_n_days_price(22)
+
+    @property
+    def last_n_days_vol(self):
+        return self.get_last_n_days_vol(22)
 
 
 def get_underestimate():
