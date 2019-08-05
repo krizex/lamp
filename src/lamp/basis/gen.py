e60899b7fc4ff307e5da6197d973e5ea21c846c3
krizex <krizex@gmail.com>
2019-08-05 11:16:52 +0800

    retry fetch stock basics
    Signed-off-by: krizex <krizex@gmail.com>

--- src/lamp/basis/gen.py
+++ src/lamp/basis/gen.py
@@ -6,13 +6,19 @@ import json
 import os
 
 def fetch_basis():
-    log.info('Fetching stock basics...')
-    pro = ts.pro_api('4105aca09e41fde2adac11ff8cdf7e05cef205d946e06935562e0010')
-    ret = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
-    log.info('Fetched')
-    df = ret[['ts_code', 'name']]
-    df = df.set_index('ts_code')
-    return df.to_json(orient='index')
+    for _ in range(10):
+        try:
+            log.info('Fetching stock basics...')
+            pro = ts.pro_api('4105aca09e41fde2adac11ff8cdf7e05cef205d946e06935562e0010')
+            ret = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+            log.info('Fetched')
+            df = ret[['ts_code', 'name']]
+            df = df.set_index('ts_code')
+            return df.to_json(orient='index')
+        except:
+            log.exception('Fetch stock basics fail, retrying...')
+
+    raise RuntimeError('Fetch stock basics fail')
 
 
 def main():
