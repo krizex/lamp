36f9f505c7e9553f4e398710d10fb6545718c85d
Yang Qian <krizex@gmail.com>
2020-02-01 21:48:57 +0800

    auto commit back up db

--- backup-db.sh
+++ backup-db.sh
@@ -2,4 +2,7 @@
 
 curl -m 60 https://cronitor.link/UyZpG1/run
 make backup-db
+if [ -f data/auto-commit.sh ];then
+  cd data && ./auto-commit.sh
+fi
 curl -m 60 https://cronitor.link/UyZpG1/complete
