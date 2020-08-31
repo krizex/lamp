2df4ad8aed2632220ac3f9260ecc2e9dcfaa0c79
Yang Qian <yang.qian@citrix.com>
2020-08-31 10:22:17 +0800

    Add default days and ratio for rebound
    Signed-off-by: Yang Qian <yang.qian@citrix.com>

--- src/lamp/model/rebound.py
+++ src/lamp/model/rebound.py
@@ -5,8 +5,8 @@ class Rebound(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(10), unique=True, nullable=False)
     name = db.Column(db.String(32))
-    days = db.Column(db.Integer, nullable=False)
-    ratio = db.Column(db.Float, nullable=False)
+    days = db.Column(db.Integer, nullable=False, default=11)
+    ratio = db.Column(db.Float, nullable=False, default=0.05)
     note = db.Column(db.Text)
 
     def __repr__(self):
